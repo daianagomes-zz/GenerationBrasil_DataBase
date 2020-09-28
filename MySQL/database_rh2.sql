04bf774ccee20a2404df7ff28423dc78747f4433
unknown <daianagomes@id.uff.br>
2020-09-28 11:17:04 -0300

    Alterações

--- MySQL/database_rh2.sql
+++ MySQL/database_rh2.sql
@@ -29,33 +29,18 @@ create table tb_cargo(
 	cargo_id bigint not null,
 
     primary key (id),
-    foreign key (cargo_id) references tb_funcionario(id)
+    foreign key (cargo_id) references tb_cargo(id)
     );
      select * from tb_funcionario; 
      
-	insert into tb_cargo(nome_cargo, nivel, salario) values ("Analista de RH", "Junior", 3500.00);
-	insert into tb_cargo(nome_cargo, nivel, salario) values ("Consultor de recursos humanos", "pleno", 7800.00);
-	insert into tb_cargo(nome_cargo, nivel, salario) values ("Assistente de departamento de pessoal", "Junior", 1500.00);
-	insert into tb_cargo(nome_cargo, nivel, salario) values ("Supervisor ou analista de cargos", "Senior", 9500.00);
-	insert into tb_cargo(nome_cargo, nivel, salario) values ("Analista de treinamento e capacitação", "pleno", 6300.00);
-
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Giovanna ", "São Paulo", '2020-05-21', 783582653);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Marisol", "Rio de Janeiro",08/15/2020, 006720627);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Hélder",  "São Paulo",04/28/2019, 49079992);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("William ", "Campinas", 03/28/2020, 452988983);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Estela ", "Campinas", 02/24/2020, 135023737); 
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Anaísa ", "São Paulo",10/04/2019, 238815914);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Abraão ", "Rio de Janeiro",04/28/2019, 537375735);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Juliana ", "Campinas", 07/21/2020, 052487162);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Haniel ","Rio de Janeiro", 04/01/2020, 018889683);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Sujana ", "São Paulo", 04/16/2020, 029644929);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Adalberto ","Rio de Janeiro", 07/15/2020, 913925160); 
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Armando ", "Rio de Janeiro", 07/15/2019, 679480186);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Giovana ", "Campinas", 01/18/2019, 253048961);
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Gael ", "Rio de Janeiro", 08/17/2020, 741292551); 
-	insert into tb_funcionario(nome, cidade, data_admissão, numero_pis) values ("Mellyssa ","Rio de Janeiro", 04/28/2019, 351723754);
-
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis,cargo_id) values ("Giovanna ", "São Paulo", '2020/05/21', 783582653,1);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Marisol", "Rio de Janeiro",'2020/08/15', 006720627, 2);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Hélder",  "São Paulo", '2019/04/28', 49079992, 3);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("William ", "Campinas", '2020/03/28', 452988983, 4);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Estela ", "Campinas", '2020/02/24', 135023737, 5); 
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Anaísa ", "São Paulo", '2019/10/04', 238815914, 6);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Abraão ", "Rio de Janeiro", '2019/04/28', 537375735, 7);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Juliana ", "Campinas", '2020/07/21', 052487162, 8);
+insert into tb_funcionario(nome, cidade, data_admissão, numero_pis, cargo_id) values ("Haniel ", "Rio de Janeiro",'2020/04/01', 769018654, 9);
   
    
-    
- 
